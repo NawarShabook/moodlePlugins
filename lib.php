39a0df866382236f753325290842aba17e6fe728
Nawar Shabook <nawarshabook@gmail.com>
2023-01-08 00:06:52 +0300

    Add plugin link to navigation

--- lib.php
+++ lib.php
@@ -43,4 +43,31 @@ function local_greetings_get_greeting($user) {
 
     return get_string($langstr, 'local_greetings', fullname($user));
 }
+/**
+* Insert a link to index.php on the site front page navigation menu.
+ *
+ * @param navigation_node $frontpage Node representing the front page in the navigation tree.
+ */
+function local_greetings_extend_navigation_frontpage(navigation_node $frontpage)
+{
+    $frontpage->add(
+        get_string('pluginname', 'local_greetings'),
+        new moodle_url('/local/greetings/index.php'),
+        navigation_node::TYPE_CUSTOM,
+        null,
+        null,
+        new pix_icon('t/message', '')
+    );
+}
 
+// funciton for Add a link to the navigation drawer
+function local_greetings_extend_navigation(global_navigation $root)
+{
+    $node = navigation_node::create(
+        get_string('pluginname', 'local_greetings'),
+        new moodle_url('/local/greetings/index.php')
+    );
+
+    $node->showinflatnavigation = true;
+    $root->add_node($node);
+}
